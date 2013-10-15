ff92afa0a064859b9c3a19fc5e9e83d22598685a
Joshua McFarland <mcfarljw@gmail.com>
2013-10-15 12:32:18 +0800

    initial commit

---
+++ docs/api.js
@@ -0,0 +1,91 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Api",
+        "Application",
+        "Assets",
+        "CanvasCharacter",
+        "CanvasStroke",
+        "CordovaAssets",
+        "Facade",
+        "Functions",
+        "HomeView",
+        "IndexedDBAdapter",
+        "InfoView",
+        "LoginView",
+        "OptionsView",
+        "PinyinConverter",
+        "Prompt",
+        "PromptCanvas",
+        "PromptDefn",
+        "PromptRdng",
+        "PromptRune",
+        "PromptTone",
+        "Recognizer",
+        "Router",
+        "SQLiteAdapter",
+        "Scheduler",
+        "Settings",
+        "Shortstraw",
+        "Stopwatch",
+        "Storage",
+        "StudyDecomp",
+        "StudyDecomps",
+        "StudyItem",
+        "StudyItems",
+        "StudyParam",
+        "StudyParams",
+        "StudyReview",
+        "StudyReviews",
+        "StudySRSConfigs",
+        "StudySentence",
+        "StudySentences",
+        "StudyStroke",
+        "StudyStrokes",
+        "StudyView",
+        "StudyVocab",
+        "StudyVocabs",
+        "User"
+    ],
+    "modules": [
+        "Collection",
+        "Facade",
+        "Model",
+        "Prompt",
+        "Skritter",
+        "Storage",
+        "View"
+    ],
+    "allModules": [
+        {
+            "displayName": "Collection",
+            "name": "Collection"
+        },
+        {
+            "displayName": "Facade",
+            "name": "Facade"
+        },
+        {
+            "displayName": "Model",
+            "name": "Model"
+        },
+        {
+            "displayName": "Prompt",
+            "name": "Prompt"
+        },
+        {
+            "displayName": "Skritter",
+            "name": "Skritter"
+        },
+        {
+            "displayName": "Storage",
+            "name": "Storage",
+            "description": "A data adapter for IndexedDB which uses the a jQuery plugin written by nparashuram.\nIt extends the Storage class to get the generic database structure and version.\nhttps://github.com/axemclion/jquery-indexeddb/"
+        },
+        {
+            "displayName": "View",
+            "name": "View"
+        }
+    ]
+} };
+});
