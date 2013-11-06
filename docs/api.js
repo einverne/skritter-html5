3f836c4df4c4efb5da4b89709149bca2768a7e43
Joshua McFarland <mcfarljw@gmail.com>
2013-11-06 09:36:30 +0800

    update to e099fddcf54971ff4109a552bfc3e24afaf6ae9f

--- docs/api.js
+++ docs/api.js
@@ -4,6 +4,7 @@ YUI.add("yuidoc-meta", function(Y) {
         "Api",
         "Application",
         "Assets",
+        "Canvas",
         "CanvasCharacter",
         "CanvasStroke",
         "Facade",
