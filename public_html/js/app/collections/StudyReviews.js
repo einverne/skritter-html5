94cc5b74d2a88a83e85252f764e0dc23e4eff606
Joshua McFarland <mcfarljw@gmail.com>
2013-12-15 16:55:50 -0500

    Un-synced reviews now add to total time

--- public_html/js/app/collections/StudyReviews.js
+++ public_html/js/app/collections/StudyReviews.js
@@ -48,29 +48,14 @@ define([
             return -review.get('submitTime');
         },
         /**
-         * @method filterBy
-         * @param {String} attribute
-         * @param {String} value
-         * @param {Boolean} checkSubString
-         * @returns {Array}
-         */
-        filterBy: function(attribute, value, checkSubString) {
-            var filtered = this.filter(function(items) {
-                if (checkSubString)
-                    return (items.get(attribute).indexOf(value) > -1) ? true : false;
-                return _.contains(value, items.get(attribute));
-            });
-            return new StudyReviews(filtered);
-        },
-        /**
          * @method getTotalTime
          * @returns {Number}
          */
         getTotalTime: function() {
             var time = 0;
-            var filtered = this.filterBy('bearTime', true);
-            for (var i in filtered.models)
-                time += parseInt(filtered.models[i].get('reviewTime'), 10);
+            for (var i in this.models)
+                if (this.models[i].get('bearTime') && skritter.moment(this.models[0].submitTime).format('YYYY[-]MM[-]DD') === skritter.settings.get('date'))
+                    time += parseInt(this.models[i].get('reviewTime'), 10);
             return time * 1000;
         },
         /**
