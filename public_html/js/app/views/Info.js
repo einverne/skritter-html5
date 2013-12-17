e851a188cdd8875eb3e3e308ffa4b97cdd67f0db
Joshua McFarland <mcfarljw@gmail.com>
2013-12-17 18:17:43 -0500

    Fixed empty decomps display

--- public_html/js/app/views/Info.js
+++ public_html/js/app/views/Info.js
@@ -55,7 +55,7 @@ define([
 
             //decompositions
             var decomps = Info.vocab.getDecomps();
-            if (decomps) {
+            if (decomps.length > 0) {
                 this.$('#decompositions tbody').html('');
                 for (var c in decomps) {
                     var decomp = decomps[c];
