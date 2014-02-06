41941fd85219b8002fcfed744b53e6fea4c2e159
Joshua McFarland <mcfarljw@gmail.com>
2014-02-06 08:58:46 -0500

    More additions to SqlLiteAdapter
    It still doesn't work and I need to set up a weinre test server to
    properly debug it.

--- public_html/js/app/models/storage/SqlLiteAdapter.js
+++ public_html/js/app/models/storage/SqlLiteAdapter.js
@@ -51,20 +51,26 @@ define(function() {
          */
         openDatabase: function(databaseName, callback) {
             SqlLiteAdapter.database = window.sqlitePlugin.openDatabase(databaseName, '1.0', databaseName, -1);
-            SqlLiteAdapter.database.transaction(populate, error, success);
+            SqlLiteAdapter.database.transaction(populate, error);
             function populate(tx) {
-                for (var name in SqlLiteAdapter.tables) {
-                    var table = SqlLiteAdapter.tables[name];
-                    var queryString = 'CREATE TABLE IF NOT EXISTS ';
-                    queryString += name + ' (' + table.keys[0] + ' PRIMARY KEY,' + table.fields.join(',') + ')';
-                    tx.executeSql(queryString);
+                var position = 0;
+                createNext();
+                function createNext() {
+                    if (position < SqlLiteAdapter.tables.length) {
+                        for (var name in SqlLiteAdapter.tables) {
+                            var table = SqlLiteAdapter.tables[name];
+                            var queryString = 'CREATE TABLE IF NOT EXISTS ';
+                            queryString += name + ' (' + table.keys[0] + ' PRIMARY KEY,' + table.fields.join(',') + ')';
+                            position++;
+                            tx.executeSql(queryString, [], createNext, error);
+                        }
+                    } else {
+                        callback();
+                    }
                 }
             }
             function error(error) {
                 alert('ERROR: ' + JSON.stringify(error));
-            }
-            function success() {
-                callback();
             }
         },
         /**
@@ -73,7 +79,19 @@ define(function() {
          * @param {Function} callback
          */
         getAll: function(tableName, callback) {
-            callback();
+            SqlLiteAdapter.database.transaction(populate, error, success);
+            function populate(tx) {
+                tx.executeSql('SELECT * FROM ' + tableName, [], querySuccess, error);
+                function querySuccess(tx, result) {
+                    callback(SqlLiteAdapter.this.parseResult(tableName, result));
+                }
+            }
+            function error() {
+                console.error('ERROR: ' + JSON.stringify(error));
+            }
+            function success() {
+                callback();
+            }
         },
         /**
          * @method getItems
@@ -107,37 +125,91 @@ define(function() {
             return stringValue;
         },
         /**
+         * @method parseResult
+         * @param {String} tableName
+         * @param {Array} result
+         * @returns {Array}
+         */
+        parseResult: function(tableName, result) {
+            var parsedResults = [];
+            for (var i = 0; i < result.rows.length; i++) {
+                var item = _.cloneDeep(result.rows.item(i));
+                //decomps
+                if (tableName === 'decomps') {
+                    item.Children = JSON.parse(item.Children);
+                }
+                //items
+                if (tableName === 'items') {
+                    item.vocabIds = JSON.parse(item.vocabIds);
+                    item.vocabListIds = JSON.parse(item.vocabListIds);
+                    item.sectionIds = JSON.parse(item.sectionIds);
+                }
+                //sentences
+                if (tableName === 'sentences') {
+                    item.containedVocabIds = item.containedVocabIds.split(',');
+                    item.definitions = JSON.parse(item.definitions);
+                }
+                //srsconfigs
+                if (tableName === 'srsconfigs') {
+                    item.rightFactors = JSON.parse(item.rightFactors);
+                    item.wrongFactors = JSON.parse(item.wrongFactors);
+                }
+                //strokes
+                if (tableName === 'strokes') {
+                    item.strokes = JSON.parse(item.strokes);
+                }
+                //vocabs
+                if (tableName === 'vocabs') {
+                    item.containedVocabIds = item.containedVocabIds.split(',');
+                    item.definitions = JSON.parse(item.definitions);
+                    if (item.topMnemonic === '') {
+                        delete item.topMnemonic;
+                    } else {
+                        item.topMnemonic = JSON.parse(item.topMnemonic);
+                    }
+                }
+                parsedResults.push(item);
+            }
+            alert('table: ' + tableName + ' results: ' + parsedResults.length);
+            return parsedResults;
+        },
+        /**
          * @method setItems
          * @param {String} tableName
          * @param {Array} items
          * @param {Function} callback
          */
         setItems: function(tableName, items, callback) {
-            SqlLiteAdapter.database.transaction(populate, error, success);
+            SqlLiteAdapter.database.transaction(populate, error);
             function populate(tx) {
+                var position = 0;
                 var table = SqlLiteAdapter.tables[tableName];
                 var queryString = 'INSERT OR REPLACE INTO ';
                 queryString += tableName + ' (' + table.keys.join(',') + ', ' + table.fields.join(',') + ')' + ' VALUES (' + SqlLiteAdapter.this.getValueString(table) + ')';
-                for (var i in items) {
-                    var item = items[i];
-                    var values = [];
-                    for (var field in table.fields) {
-                        var fieldValue = item[field];
-                        if (typeof fieldValue === 'undefined') {
-                            fieldValue = '';
-                        } else if (fieldValue.constructor === Array || fieldValue.constructor === Object) {
-                            fieldValue = JSON.stringify(fieldValue);
+                setNext();
+                function setNext() {
+                    if (position < items.length) {
+                        var item = items[position];
+                        var values = [];
+                        for (var b in table.fields) {
+                            var value = item[table.fields[b]];
+                            if (typeof value === 'undefined') {
+                                value = '';
+                            } else if (value.constructor === Array || value.constructor === Object) {
+                                value = JSON.stringify(value);
+                            }
+                            values.push(value);
                         }
-                        values.push(fieldValue);
+                        position++;
+                        tx.executeSql(queryString, values, setNext, error);
+                    } else {
+                        if (typeof callback === 'function')
+                            callback();
                     }
-                    tx.executeSql(queryString, values, null, error);
                 }
             }
             function error(error) {
-                alert('ERROR: ' + JSON.stringify(error));
-            }
-            function success() {
-                callback();
+                console.error('ERROR: ' + JSON.stringify(error));
             }
         }
     });
