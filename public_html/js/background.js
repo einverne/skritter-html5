a902d49b6ec7cae4f1cb45892565ad333c8bf78a
Joshua McFarland <mcfarljw@gmail.com>
2014-02-10 11:46:09 -0500

    More changes for better google app support

--- public_html/js/background.js
+++ public_html/js/background.js
@@ -1,8 +1,8 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
     'bounds': {
-      'width': 800,
-      'height': 800
+      'width': window.screen.width,
+      'height': window.screen.height
     }
   });
 });
--- public_html/js/tracking.js
+++ public_html/js/tracking.js
@@ -27,7 +27,7 @@ _gaq.push(['_trackPageview']);
     var ga = document.createElement('script');
     ga.type = 'text/javascript';
     ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    ga.src = ('js/libs/ga.min.js');
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(ga, s);
 })();
